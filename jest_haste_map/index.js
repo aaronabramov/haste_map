fd4e5d30fc225048a050c6f18bd685125fa96185
Jordan Eldredge <jordan@jordaneldredge.com>
2018-05-21 16:43:00 +0100

    Build artifact in JS

--- jest_haste_map/index.js
+++ jest_haste_map/index.js
@@ -1,6 +1,10 @@
 const HasteMap = require("jest-haste-map");
 const path = require("path");
 const os = require("os");
+const fs = require("fs");
+
+const DEPENDENCY_INDEX = 3;
+const ARTIFACT_PATH = "../haste_map_js.txt";
 
 const hasteMap = new HasteMap({
   name: "test",
@@ -18,4 +22,17 @@ console.time("TOP LEVEL TIME WRAPPER");
 hasteMap.build().then(m => {
   console.timeEnd("TOP LEVEL TIME WRAPPER");
   console.log(Object.keys(m.hasteFS._files).length);
+
+  if (fs.existsSync(ARTIFACT_PATH)) {
+    console.log("Artifact already exists");
+  } else {
+    console.log("Artifact not found creating...");
+    const files = m.hasteFS._files;
+    const artifact = Object.keys(files)
+      .map(file => `${file}|${files[file][DEPENDENCY_INDEX].sort().join("|")}`)
+      .sort()
+      .join("\n");
+
+    fs.writeFileSync(ARTIFACT_PATH, artifact);
+  }
 });
