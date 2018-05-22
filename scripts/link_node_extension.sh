e12b19d8e7d27a94938407c0eef8aeea484ba112
Jordan Eldredge <jordan@jordaneldredge.com>
2018-05-22 09:45:53 +0100

    Allow build to work first time

--- scripts/link_node_extension.sh
+++ scripts/link_node_extension.sh
@@ -6,12 +6,13 @@ source "$BASEDIR""/scripts/env.sh"
 
 TARGET_DIR='debug'
 BUILD_DIR="$BASEDIR""/build"
-NODE_EXT=$(realpath "$BUILD_DIR""/libnodejs_extension.node")
+NODE_EXT=$(realpath -m "$BUILD_DIR""/libnodejs_extension.node")
 
 if [ "$1" == "release" ]; then TARGET_DIR='release'; fi
 
 DLYB=$(realpath "$BASEDIR""/target/""$TARGET_DIR""/libnodejs_extension.dylib")
 
 rm -f $NODE_EXT
+mkdir -p $BUILD_DIR
 
 cp $DLYB $NODE_EXT  && printf "[  ""$GREEN""OK""$NC""  ] Create ""$BLUE""$NODE_EXT""$NC""\n"
